365d9052010f231f3674d084e456961d477676f2
PinkNanie <nathalie.pouvrasseau.t@gmail.com>
2020-03-20 14:35:55 +0100

    méthode getNeighboursFavorite

--- app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
@@ -1,7 +1,12 @@
 package com.openclassrooms.entrevoisins.service;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ListFragment;
+
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,10 +43,23 @@ public class DummyNeighbourApiService implements  NeighbourApiService {
         neighbours.add(neighbour);
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
-    public List<Neighbour> setNeighbourFavorite(Neighbour neighbour, boolean isFavorite) {
-        return null;
+    public List<Neighbour> getNeighboursFavorite() {
+
+        List<Neighbour> neighboursFavorite = new ArrayList<>();
+
+        for (Neighbour neighbour : neighbours){
+            if (neighbour.isFavorite()) {
+                neighboursFavorite.add(neighbour);
+            }
+        }
+        return neighboursFavorite;
     }
+
+    @Override
+    public void reverseNeighbour(Neighbour neighbour) {
 
 }
 
