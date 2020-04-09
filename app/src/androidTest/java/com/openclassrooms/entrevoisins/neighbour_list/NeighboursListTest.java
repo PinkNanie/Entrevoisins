8dd37236989a3531e9eb1c7261fe4bd8fb9cb295
PinkNanie <nathalie.pouvrasseau.t@gmail.com>
2020-04-09 10:36:16 +0200

    grouping tests on Neighbour_list

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -1,25 +1,40 @@
 
 package com.openclassrooms.entrevoisins.neighbour_list;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 
@@ -69,5 +84,118 @@ public class NeighboursListTest {
         onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
     }
 
+    /**
+     * When we clicked on element view, show new activity with details of neighbour
+     */
 
+    @Test
+    public void detailsNeighbour_Firstname() {
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.detailsNeighbour_btn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.list_neighbours),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.firstname), withText("Caroline"),
+                        childAtPosition(
+                                allOf(withId(R.id.Activity_Toolbar_Details),
+                                        childAtPosition(
+                                                withId(R.id.toolbar),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+    }
+
+    /**
+     * Favorite's Tab, show Favorite List
+     */
+
+    @Test
+    public void myFavoriteListTest() {
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.detailsNeighbour_btn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.list_neighbours),
+                                        2),
+                                3),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.favorite_btn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.return_btn),
+                        childAtPosition(
+                                allOf(withId(R.id.Activity_Toolbar_Details),
+                                        childAtPosition(
+                                                withId(R.id.toolbar),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction tabView = onView(
+                allOf(withContentDescription("Favorites"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tabs),
+                                        0),
+                                1),
+                        isDisplayed()));
+        tabView.perform(click());
+
+        ViewInteraction viewPager = onView(
+                allOf(withId(R.id.container),
+                        childAtPosition(
+                                allOf(withId(R.id.main_content),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        viewPager.perform(swipeLeft());
+
+        ViewInteraction viewGroup = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.favList_neighbours),
+                                withParent(withId(R.id.container))),
+                        0),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
+    }
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
